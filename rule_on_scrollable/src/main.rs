609264af606468993c5d1c21a30f13630f0ce7cb
TheComamba <thecomamba@jojoheinze.de>
2024-01-05 09:16:07 +0100

    Simplifying example.

--- rule_on_scrollable/src/main.rs
+++ rule_on_scrollable/src/main.rs
@@ -29,37 +29,40 @@ impl Sandbox for Gui {
         let rule_direction = Direction::Horizontal(Properties::default());
 
         let simple_rule = Column::new()
-            .push(Text::new("Simple Rule is ok"))
+            .push(Text::new("A simple Rule without a Scrollable is ok."))
             .push(Rule::horizontal(20.));
+
+        let simple_scrollable = Column::new().push(
+            Scrollable::new(Text::new(
+                "A simple Scrollable without any Rule is happy as well.",
+            ))
+            .direction(rule_direction)
+            .width(iced::Length::Fixed(300.)),
+        );
+
+        let rule_on_scrollable_fixed = Column::new()
+            .push(Text::new(
+                "A lonely Rule on a Scrollable with fixed width is invisible, while the Scrollable content is unfathomably large (note the tiny scrollbar). Additionally, this text has linebreaks instead of being scrollable.",
+            ))
+            .push(Scrollable::new(Rule::horizontal(20.)).direction(rule_direction))
+            .width(iced::Length::Fixed(300.));
 
         let rule_on_scrollable = Column::new()
-            .push(Text::new("A lonely Rule on a Scrollable is invisible"))
-            .push(Scrollable::new(Rule::horizontal(20.)).direction(rule_direction));
-
-        let rule_and_text = Column::new()
-            .push(Text::new("Rule and Text on Scrollable is ok"))
-            .push(Rule::horizontal(20.));
-        let rule_and_text_on_scrollable = Scrollable::new(rule_and_text);
-
-        let table_header = Text::new(
-            "A rather lengthy text to demonstrate that the scrollabel does or does not appear. Again: A rather lengthy text to demonstrate that the scrollabel does or does not appear.",
-        );
-        let table = Column::new().push(table_header); //.push(Rule::horizontal(10));
-        let direction = Direction::Both {
-            vertical: Properties::default(),
-            horizontal: Properties::default(),
-        };
-        let bla = Scrollable::new(table)
-            .direction(direction)
+            .push(Text::new(
+                "The situation is the same if the Scrollable has a Fill width. The Scrollable content is unfathomably large, and this text has linebreaks instead of being scrollable.",
+            ))
+            .push(Scrollable::new(Rule::horizontal(20.)).direction(rule_direction))
             .width(iced::Length::Fill);
 
         Column::new()
             .push(simple_rule)
+            .push(simple_scrollable)
+            .push(rule_on_scrollable_fixed)
             .push(rule_on_scrollable)
-            .push(rule_and_text_on_scrollable)
-            .push(bla)
             .width(iced::Length::Fill)
             .height(iced::Length::Fill)
+            .spacing(20.)
+            .padding(20.)
             .into()
     }
 }
